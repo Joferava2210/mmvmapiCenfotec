5bbd99a64a5e5cc36bbf10e67bf5d74f4ef5f877
joferava <jfelipe221095@gmail.com>
2021-03-06 08:30:21 -0600

    Initial Commit

---
+++ mmvmapi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mmvmapi
+//
+//  Created by Felipe Ramirez Vargas on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mmvmapi/mmvmapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmvmapiApp.swift
+//  mmvmapi
+//
+//  Created by Felipe Ramirez Vargas on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct mmvmapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
